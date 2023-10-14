39513ae325d1bfa4a7df4199d53731206aa0d6da
CriminalButterrr <h.agwiking@gmail.com>
2023-10-14 19:08:01 +0800

    check niyo nalang mga pre

---
+++ LabAct4/Player.cs
@@ -0,0 +1,22 @@
+class Player:Character
+{
+    private bool canUseHeal = true;
+    public Player(string name): base()
+    {
+        Name = name;
+    }
+
+    public void Heal()
+    {
+        if (canUseHeal)
+        {
+            int healAmount = (int)(0.2 * Health);
+            Health += healAmount;
+            canUseHeal = false;
+        }
+        else
+        {
+            Console.WriteLine("You can't use heal right now. Wait for your next turn.");
+        }
+    } 
+}
